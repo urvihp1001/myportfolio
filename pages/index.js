b5768d496d89f5d5f8997f96f9a67270ab1c5a44
Urvi Patel <128279752+urvihp1001@users.noreply.github.com>
2023-11-22 19:35:04 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import styles from '../styles/Home.module.css';
+import Chatbot from 'react-chatbot-kit';
+
+import config  from "../utility/chatbot/config";
+import ActionProvider from "../utility/chatbot/ActionProvider";
+import MessageParser from "../utility/chatbot/MessageParser";
+import LinksBar from "../components/layouts/LinksBar";
